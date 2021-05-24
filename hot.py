4b38fd876310bf6faa35d1b4e2f5b73d29bae967
mac <sutdcook>
2021-05-24 11:47:09 +0300

    Initial commit

---
+++ hot.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+with sqlite3.connect('bd/database.db') as hot:
+
+    cursor = hot.cursor()
+    query = """CREATE TABLE IF NOT EXISTS cookbd (ID,cook,dish,'qq',url)"""
+    cursor.execute(query)
+    chiken = """INSERT INTO cookbd ('ID','cook','dish','qq','url') VALUES ('113','Рецепт курицы в сковороде','ингридиенты:1 кг курицы,200 грамм сметаны,3 больших луковицы,300 мл горячей воды,специи,соль,перец','сковородка','видео готовки:https://www.youtube.com/watch?v=K48subsADYU')"""
+    cursor.execute(chiken)
+    chiken2 = """INSERT INTO cookbd ('ID','cook','dish','url') VALUES ('112','Рецепт курицы в духовке','ингридиенты:Куриная тушка,молотая паприка,Базилик,Оливковое масло,cоль и перец','https://www.youtube.com/watch?v=x0wVay7JGjY')"""
+    cursor.execute(chiken2)
+    soupc = """INSERT INTO cookbd ('ID','cook','dish','url') VALUES ('444','Рецепт куриного супа','Ингредиенты:Курица,Картофель - 500 г,Морковь - 160 г,Лук репчатый - 200 г,Чеснок - 3 зубчика,Масло растительное - 1 ст.л.,Масло сливочное - 30 г,Мука пшеничная - 150 г,Лавровый лист - 1 шт,Смесь сушеных овощей - 1 ст.л.,Куриные яйца - 2 шт,Вода  - 4 ст.л.,Свежая петрушка - 5 г,Зеленый лук - по вкусу,Куркума молотая - 1/2 ч.л.,Перец черный молотый - 1/2 ч.л.,Соль - 2,5 ч.л.','https://www.youtube.com/watch?v=V_Pni2Lx7bY')"""
+    cursor.execute(soupc)
+    soupb = """INSERT INTO cookbd ('ID','cook','dish','url') VALUES ('555','Рецепт борща','ингридиенты (на 2 л бульона):500 г Мясо на косточке,200 г свеклы,150 г капусты,110 г картофеля,100 г моркови,100 г лука,2 ст.л. томатного пюре,3 ч.л. уксуса 9%,3 ч.л. сахара,Чеснока,зелень,соль,перец болгарский','https://www.youtube.com/watch?v=ur_lfpcwjAY')"""
+    cursor.execute(soupb)
+    soupw = """INSERT INTO cookbd ('ID','cook','dish','url') VALUES ('888','рецепт Шурпы','Ингредиенты:говядина - 600гр.,горох нут - 80гр.,лук репчатый - 2шт.,лук белый - 1шт.,морковь - 3шт.,сладкий перец - 2шт.,помидор - 3шт.,картофель - 6 - 7шт.,чеснок - 5 зубков,масло топленое - 1.5ст.л.,кориандр - 0.5,ч.л.,кумин - 0.5 ч.л.,соль по вкусу,кипяток - 1.5 - 2л.','https://www.youtube.com/watch?v=7VpKhhr38V8')"""
+    cursor.execute(soupw)
+    wawlik = """INSERT INTO cookbd ('ID','cook','dish','url') VALUES ('999','рецепт шашлыка из свинины','Свинина 1кг,Приправа к шашлыку 15гр,Лук – 1-2шт,Растительное масло 1ст.л,Соль','https://www.youtube.com/watch?v=IxdOYS7A9OU')"""
+    cursor.execute(wawlik)
+    salat1 = """INSERT INTO cookbd ('ID','cook','dish','url') VALUES ('931','рецепт горячего салата:','400 г говядина,200 г огурцы,1 морковь,1 лук репчатый,250 г перец сладкий,2 ст.л кинзы,2-3 зубчика чеснока,1 ч.л сахар,1 ст.л рисовый уксус или 1 ч.л яблочный или любой,4 ст.л соевый соус,3 ст.л рстительное масло + 1 ст.л кунжутного,1 ст.л кунжут,красный перец по вкусу ','https://www.youtube.com/watch?v=lc-39kH7p4Q')"""
+    cursor.execute(salat1)
+    salat2 = """INSERT INTO cookbd ('ID','cook','dish','url') VALUES ('47','рецепт холодного салата:','400 г говядина,200 г огурцы,1 морковь,1 лук репчатый,250 г перец сладкий,2 ст.л кинзы,2-3 зубчика чеснока,1 ч.л сахар,1 ст.л рисовый уксус или 1 ч.л яблочный или любой,4 ст.л соевый соус,3 ст.л рстительное масло + 1 ст.л кунжутного,1 ст.л кунжут,красный перец по вкусу ','https://www.youtube.com/watch?v=bDD6I5M_EdE')"""
+    cursor.execute(salat2)
+    svinyadux = """INSERT INTO cookbd ('ID','cook','dish','url') VALUES ('15','рецепт свинины в духовке:','1,2 кг гудинки,2/3 ст.л соли,1 сn.л паприка,1 ч.л черный перец,1 ч.л кориандр,2 сn.л острый кетчуп или аджика + 1 ч/л меда +т1/2 ч/л паприка,Запекать 180С (360Ф) 40 мин + 20 мин с глазурью ','https://www.youtube.com/watch?v=Fl6M52kCyMs')"""
+    cursor.execute(svinyadux)
+    ggv = """INSERT INTO cookbd ('ID','cook','dish','url') VALUES ('59','рецепт говядины на сковороде:','Ингредиенты:Мякоть говядины - 1 кг,Лук репчатый - 150 г,Морковь - 150 г,Чеснок - 4 зубчика,Томатная паста - 2 ст.л.,Сливочное масло - 40 г,Растительное масло - 2 ст.л.,Мука пшеничная - 2 ст.л.,Соль - 1 ч.л.,Перец черный молотый - ½ ч.л.,Паприка сладкая - 1 ч.л.,Кориандр молотый - 1 ч.л.,Вода кипяток - 800 мл,Петрушка -  10 г,Зеленый лук - 10 г ','https://www.youtube.com/watch?v=3itytdo2dDk')"""
+    cursor.execute(ggv)
+    ggd = """INSERT INTO cookbd ('ID','cook','dish','url') VALUES ('228','рецепт говядины в духове:','говядина 3кг,оливовое масло 30мл,соль 10гр,черный перец 10гр','https://www.youtube.com/watch?v=b4yLP2ge4V4')"""
+    cursor.execute(ggd)
+    a = cursor.execute(f"SELECT cook , dish , url FROM cookbd WHERE ID = '{113}'").fetchone()
+    b = cursor.execute(f"SELECT cook , dish , url FROM cookbd WHERE ID = '{112}'").fetchone()
+    c = cursor.execute(f"SELECT cook , dish , url FROM cookbd WHERE ID = '{444}'").fetchone()
+    d = cursor.execute(f"SELECT cook , dish , url FROM cookbd WHERE ID = '{555}'").fetchone()
+    e = cursor.execute(f"SELECT cook , dish , url FROM cookbd WHERE ID = '{888}'").fetchone()
+    h = cursor.execute(f"SELECT cook , dish , url FROM cookbd WHERE ID = '{999}'").fetchone()
+    y = cursor.execute(f"SELECT cook , dish , url FROM cookbd WHERE ID = '{931}'").fetchone()
+    p = cursor.execute(f"SELECT cook , dish , url FROM cookbd WHERE ID = '{47}'").fetchone()
+    z = cursor.execute(f"SELECT cook , dish , url FROM cookbd WHERE ID = '{15}'").fetchone()
+    l = cursor.execute(f"SELECT cook , dish , url FROM cookbd WHERE ID = '{59}'").fetchone()
+    o = cursor.execute(f"SELECT cook , dish , url FROM cookbd WHERE ID = '{228}'").fetchone()
---
+++ main.py
@@ -0,0 +1,159 @@
+import telebot
+import config
+import hot
+
+
+
+from telebot import types
+
+bot = telebot.TeleBot(config.TOKEN)
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+
+# keyboard
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Я знаю что я хочу")
+#bot.send_message(message.chat.id,message.text)
+
+
+    item2 = types.KeyboardButton("Я не знаю что я хочу")
+
+    markup.add(item1, item2)
+
+
+
+    bot.send_message(message.chat.id,
+                     "Добро пожаловать, {0.first_name}!\nЯ - <b>{1.first_name}</b>, этот бот созданный чтобы помогать вам в готовке.".format(
+                         message.from_user, bot.get_me()),
+                     parse_mode='html', reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def x(message):
+    if message.chat.type == 'private':
+        if message.text == 'Я знаю что я хочу':
+            bot.send_message(message.chat.id, 'что вы хотите?')
+
+
+
+
+        elif message.text == 'Я не знаю что я хочу':
+
+            markup = types.InlineKeyboardMarkup(row_width=3)
+            item1 = types.InlineKeyboardButton("Супы", callback_data='1')
+            bot.send_message(message.chat.id, 'вот что могу предложить ')
+            item2 = types.InlineKeyboardButton("Горячие блюда", callback_data='2')
+            item3 = types.InlineKeyboardButton("Салаты", callback_data='3')
+
+            markup.add(item1, item2, item3)
+
+            bot.send_message(message.chat.id, 'смотри', reply_markup=markup)
+
+
+        else:
+            bot.send_message(message.chat.id, 'Я не знаю что вам ответить 😢')
+
+
+
+
+@bot.callback_query_handler(func=lambda call: True)
+
+def callback_inline(call):
+    try:
+        if call.message:
+
+            if call.data == '1':
+                markup = types.InlineKeyboardMarkup(row_width=4)
+                itemz = types.InlineKeyboardButton("куриный", callback_data='444')
+                itemx = types.InlineKeyboardButton("борщ", callback_data='555')
+                itemc = types.InlineKeyboardButton("шурпа", callback_data='888')
+
+                markup.add(itemz, itemx,itemc)
+                bot.send_message(call.message.chat.id, 'какой суп вы хотите?', reply_markup=markup)
+
+            elif call.data == '2':
+                markup = types.InlineKeyboardMarkup(row_width=4)
+                itemq = types.InlineKeyboardButton("сковорода", callback_data ='666')
+                itemw = types.InlineKeyboardButton("духовка", callback_data ='777')
+                markup.add(itemq, itemw)
+                bot.send_message(call.message.chat.id, 'в чем будем готовить?', reply_markup=markup)
+            if call.data == '666':
+
+                markup = types.InlineKeyboardMarkup(row_width=4)
+                item5 = types.InlineKeyboardButton("курица", callback_data='111')
+                item6 = types.InlineKeyboardButton("говядина", callback_data='222')
+                item7 = types.InlineKeyboardButton("свинина", callback_data='333')
+                markup.add(item5, item6, item7)
+                bot.send_message(call.message.chat.id, 'смотри', reply_markup=markup)
+
+            if call.data =='777':
+                markup = types.InlineKeyboardMarkup(row_width=4)
+                item5 = types.InlineKeyboardButton("курица", callback_data='121')
+                item6 = types.InlineKeyboardButton("говядина", callback_data='221')
+                item7 = types.InlineKeyboardButton("свинина", callback_data='133')
+                markup.add(item5, item6, item7)
+                bot.send_message(call.message.chat.id, 'смотри', reply_markup=markup)
+            if call.data == '133':
+                bot.send_message(call.message.chat.id, str(hot.z))
+            if call.data == '333':
+                    bot.send_message(call.message.chat.id, str(hot.h))
+            if call.data == '221':
+                    bot.send_message(call.message.chat.id, str(hot.o))
+
+
+
+            elif call.data == '3':
+                markup = types.InlineKeyboardMarkup(row_width=4)
+                item0 = types.InlineKeyboardButton("горячие", callback_data='424')
+                item01 = types.InlineKeyboardButton("холодные", callback_data='721')
+
+                markup.add(item0, item01)
+                bot.send_message(call.message.chat.id, 'какие салаты вы хотите?', reply_markup=markup)
+
+        if call.data == '424':
+            bot.send_message(call.message.chat.id, str(hot.y))
+        if call.data == '721':
+            bot.send_message(call.message.chat.id, str(hot.p))
+
+        if call.data == '222':
+                bot.send_message(call.message.chat.id, str(hot.l))
+
+
+
+
+
+        if call.data == '111':
+            bot.send_message(call.message.chat.id, str(hot.a))
+
+
+        if call.data == '121':
+             bot.send_message(call.message.chat.id, str(hot.b))
+        if call.data == '444':
+                        bot.send_message(call.message.chat.id, str(hot.c))
+        if call.data == '555':
+            bot.send_message(call.message.chat.id, str(hot.d))
+        if call.data == '888':
+                bot.send_message(call.message.chat.id, str(hot.e))
+
+
+
+
+
+
+    #finally:
+       # pass
+
+
+
+
+
+
+
+    except Exception as e:
+                print(repr(e))
+
+
+# RUN
+bot.polling(none_stop=True)
+
